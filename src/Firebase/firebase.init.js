a707658592d695b454d698fa9150fb2b296158c3
TCahnaf <ahnafchwd164@gmail.com>
2025-11-09 20:59:29 -0500

    added protected Routes

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -1,19 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD8YK9VSKGVvgRZoNOk0B5gIBEtiLPaS6k",
-  authDomain: "utility-mangement-react.firebaseapp.com",
-  projectId: "utility-mangement-react",
-  storageBucket: "utility-mangement-react.firebasestorage.app",
-  messagingSenderId: "971344021721",
-  appId: "1:971344021721:web:0ecb10e5a1adefd90256c8"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export default app
+export default app;
